4419df0965c2409367cc4852f6a02fe14889b3f3
ergeakova <ergeak@gmail.com>
2022-04-22 02:06:54 +0300

    Initial Commit

---
+++ LandmarkBookProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBookProject
+//
+//  Created by Erge Gevher Akova on 22.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
